8f4af4ff9b52ee9dd4e01ce17c748ec13d2400e1
glm729 <georgemalone@outlook.com.au>
2022-01-06 15:45:54 +0800

    Almost-working prototype of download handler
    - Does not yet check speed
      - Also have no idea what to do if the download is too slow
      - Probably handling a custom exception
    - Add Video class for finer control of video data
    - Pass download handler instance into download handler logger
      - Bit circular, but provides necessary control based on debug messages
    - Add percentage check method
    - Add method for storing video data

--- ytdlp_utils/download_handler.py
+++ ytdlp_utils/download_handler.py
@@ -5,6 +5,7 @@
 # -----------------------------------------------------------------------------
 
 
+import time
 import yt_dlp
 
 from message_handler import MessageHandler
@@ -16,6 +17,58 @@ from message_handler import MessageHandler
 
 class InterruptSpeed(Exception):
     pass
+
+
+# Helper class definitions
+# -----------------------------------------------------------------------------
+
+
+class Video:
+
+    _prefix_template = "\033[1;{c}m{i}\033[0m"
+
+    def __init__(self, video_id: str, colour_index: int = 37):
+        prefix = self._prefix_template.format(c=colour_index, i=video_id)
+        defaults = (
+            ("count_restart", 0),
+            ("count_slow", 0),
+            ("id", video_id),
+            ("prefix", prefix),
+            ("progress", 0),
+            ("stage", 0),
+            ("time_start", None))
+        for (k, v) in defaults:
+            setattr(self, k, v)
+
+    # ---- Public methods
+
+    def decrement_stage(self):
+        """Decrement the instance stage"""
+        self.stage -= 1
+
+    def get_stage_text(self, lower=False):
+        """Return text for the current video download stage
+
+        @param lower Should the text be returned as lowercase?
+        """
+        if self.stage == 0:
+            text = "Video"
+        elif self.stage == 1:
+            text = "Audio"
+        else:
+            text = "Unknown stage"
+        if lower:
+            return text.lower()
+        return text
+
+    def increment_stage(self):
+        """Increment the instance stage"""
+        self.stage += 1
+        self.progress = 0
+
+    def set_time_start(self):
+        """Set the `time_start` attribute for the instance"""
+        self.time_start = time.time()
 
 
 # Custom logger class definition
@@ -30,16 +83,12 @@ class DownloadHandlerLogger:
         "Requested formats are incompatible for merge ",
         "and will be merged into mkv"))
 
-    _skip_hints = (
-        "Deleting original file",
-        "[download]",
-        "[info]",
-        "[youtube]")
+    _skip_hints = ("Deleting original file", "[info]")
 
     # ---- Constructor
 
-    def __init__(self, message_handler: MessageHandler):
-        self._message_handler = message_handler
+    def __init__(self, download_handler):
+        self._handler = download_handler
 
     # ---- Public methods (for yt_dlp.YoutubeDL)
 
@@ -50,8 +99,36 @@ class DownloadHandlerLogger:
         """
         if any(map(lambda x: message.startswith(x), self._skip_hints)):
             return
-        if message.startswith("[Merger] Merging formats into"):
-            self._message("Merging", "data")
+        if message.startswith(y := "[youtube]"):
+            video_id = message.split(":")[0].split(" ")[1]
+            if video_id != (c := self._handler._current_video):
+                if c is not None:
+                    previous = self._handler._get_current_video_data()
+                    text = "{p}: Video downloaded and merged in {t:.1f}s"
+                    self._message(
+                        text.format(
+                            p=previous.prefix,
+                            t=time.time() - previous.time_start),
+                        "ok")
+                self._handler._set_current_video(video_id)
+                text = "{p}: Starting download".format(
+                    p=self._handler._get_current_video_data().prefix)
+                self._message(text, "ok")
+                self._handler._video_data.get(video_id).set_time_start()
+            return
+        if message.startswith("[download] Destination:"):
+            video_current = self._handler._get_current_video_data()
+            text = "{p}: Downloading {s}".format(
+                p=video_current.prefix,
+                s=video_current.get_stage_text(lower=True))
+            self._message(text, "info")
+            return
+        if message.startswith("[Merger]"):
+            text = "{p}: Merging data".format(
+                p=self._handler._get_current_video_data().prefix)
+            self._message(text, "info")
+            return
+        if message.startswith("[download]"):
             return
         self._message(message, "data")
 
@@ -74,8 +151,8 @@ class DownloadHandlerLogger:
 
         @param message Warning message text.
         """
+        # Ignore the merge warning
         if message == self._merge_warning:
-            self._message("Formats will be merged", "warn")
             return
         self._message(message, "warn")
 
@@ -87,7 +164,7 @@ class DownloadHandlerLogger:
         @param text Message text.
         @param form Message form.
         """
-        self._message_handler.message(text=text, form=form)
+        self._handler._message_handler.message(text=text, form=form)
 
 
 # Class definition
@@ -111,36 +188,70 @@ class DownloadHandler:
 
     # ---- Constructor
 
-    def __init__(self):
-        pass
+    def __init__(self, video_ids):
+        self._store_video_data(video_ids)
 
     # ---- Public methods
 
-    def run(self, links) -> None:
-        """Main public run method
-
-        @param links Video links to attempt to download.
-        """
+    def run(self) -> None:
+        """Main public run method"""
         self._init_message_handler()
+        self._logger = DownloadHandlerLogger(self)  # wooo
         ytdlp_opts = self._ytdlp_options.copy()
         ytdlp_opts.update({
-            "logger": DownloadHandlerLogger(self._message_handler),
+            "logger": self._logger,
             "progress_hooks": [
                 self._progress_hook_downloading,
                 self._progress_hook_finished
             ],
         })
-        self._message("Starting", "ok")
+        self._message("Starting operations", "ok")
+        time_start = time.time()
         with yt_dlp.YoutubeDL(ytdlp_opts) as yt:
-            yt.download(links)
-        self._message("Ending", "ok")
+            yt.download(self._video_data.keys())
+        last = self._get_current_video_data()
+        text_last = "{p}: Video downloaded and merged in {t:.1f}s".format(
+            p=last.prefix,
+            t=time.time() - last.time_start)
+        self._message(text_last, "ok")
+        text_end = "Videos downloaded and merged in {t:.1f}s"
+        self._message(text_end.format(t=time.time() - time_start), form="ok")
         self._end_message_handler()
 
     # ---- Private methods
 
+    def _check_percentage(self, video_id: str, percentage: int) -> None:
+        """Check percentage for the current download
+
+        @param video_id ID of the current video download.
+        @param percentage Integer percentage from the current info dict.
+        """
+        data = self._video_data.get(video_id)
+        floor_20_pc = percentage - (percentage % 20)
+        if floor_20_pc >= (data.progress + 20):
+            text = "{p}: {s} download reached {v}% ({t:.1f}s)".format(
+                p=data.prefix,
+                s=data.get_stage_text(),
+                v=str(floor_20_pc).rjust(3, " "),
+                t=time.time() - data.time_start)
+            self._message(text, "info")
+            self._video_data.get(video_id).progress = floor_20_pc
+
+    def _check_speed(self, speed: float) -> None:
+        """Check speed of the current download
+
+        @param speed Speed of the download in bytes per second, from the
+        current info dict.
+        """
+        pass
+
     def _end_message_handler(self) -> None:
         """Close the instance MessageHandler"""
         self._message_handler.end()
+
+    def _get_current_video_data(self):
+        """Return the dataset for the current video ID"""
+        return self._video_data.get(self._current_video)
 
     def _init_message_handler(self) -> None:
         """Instantiate and start the instance MessageHandler"""
@@ -163,8 +274,13 @@ class DownloadHandler:
         """
         if not data.get("status") == "downloading":
             return
-        self._message("Downloading...", "info")
-        # TODO: This hook could be used to check download progress!
+        video_id = data.get("info_dict").get("id")
+        total_bytes = data.get("total_bytes")
+        current_bytes = data.get("downloaded_bytes")
+        speed = data.get("speed")
+        percentage = round((current_bytes / total_bytes) * 100)
+        self._check_percentage(video_id, percentage)
+        self._check_speed(speed)
 
     def _progress_hook_finished(self, data) -> None:
         """Custom progress hook for the "finished" status
@@ -174,8 +290,32 @@ class DownloadHandler:
         """
         if not data.get("status") == "finished":
             return
-        self._message("Finished stage!", "ok")
-        # TODO: This hook could be used to increment the stage!
+        video_id = data.get("info_dict").get("id")
+        self._video_data.get(video_id).increment_stage()
+
+    def _set_current_video(self, video_id: str) -> None:
+        """Set the current video ID for the instance
+
+        @param video_id Video ID to set as the current video.
+        """
+        self._current_video = video_id
+
+    def _store_video_data(self, video_ids) -> None:
+        """Store a structured set of video data in the instance
+
+        @param video_ids List or tuple of youtube video IDs.
+        """
+        colour_index = 30
+        data = {}
+        template = "\033[1;{c}m{i}\033[0m"
+        for video_id in video_ids:
+            data.update({ video_id: Video(video_id, colour_index) })
+            if colour_index >= 37:
+                colour_index = 30
+                continue
+            colour_index += 1
+        self._video_data = data
+        self._current_video = None
 
 
 # Operations
@@ -183,5 +323,5 @@ class DownloadHandler:
 
 
 if __name__ == "__main__":
-    dh = DownloadHandler()
-    dh.run(["A7xhz56RaxY"])
+    dh = DownloadHandler(["A7xhz56RaxY", "ZdjcHv8xkjg"])
+    dh.run()
