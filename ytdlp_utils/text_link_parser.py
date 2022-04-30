28a10254dc81359af731020e07ee1e362e66259d
George L. Malone <georgemalone@outlook.com.au>
2022-04-30 11:39:05 +0800

    Rewrite text link parsing
    * Mostly function definitions, but class-based wrapper provided
    
    * Read a text file and extract video IDs:
      * Read file contents, split by newline, and strip whitespace
      * Reduce to lines which are not comments (using prefixed hash symbol)
        and are not empty
      * Remove trailing comments on lines, if any
      * Search for IDs in each line using a list (or tuple) of regular
        expressions bearing the `id` capture group
    
    * Default regular expressions include normal, shorts, and compact URI
      structures
    
    * Provide a class wrapper for operations:
      * Requires path to the video links file
      * Extracts, stores, and returns video ID data
    
    TODO: Provide updated example usage

--- ytdlp_utils/text_link_parser.py
+++ ytdlp_utils/text_link_parser.py
@@ -1,91 +1,179 @@
-#!/usr/bin/env python3.8
+#!/usr/bin/env python3.10
 
 
 # Module imports
 # -----------------------------------------------------------------------------
 
 
+import argparse
+import functools
 import re
 
-from message import Message
+
+# Constants
+# -----------------------------------------------------------------------------
 
 
-# Class definition
+REX_DEFAULT = [
+    re.compile(r"[\?&]v\=(?P<id>[^\?&]+)"),                # Normal
+    re.compile(r"youtube\.com\/shorts\/(?P<id>[^\?&]+)"),  # "Shorts"
+    re.compile(r"youtu\.be\/(?P<id>[\?&]+)"),              # Compact
+]
+
+
+# Function definitions
+# -----------------------------------------------------------------------------
+
+
+def extract_from(data: list, rex: list) -> list:
+    """Extract Youtube video IDs from a given dataset and regex dict
+
+    @param data List of text in which to search for Youtube IDs
+    @param rex List or tuple of regular expressions to use for searches
+    @return List of Youtube video IDs found in the data
+    """
+    func = functools.partial(reduce_result, rex=rex)
+    return functools.reduce(func, data, [])
+
+
+def extract_from_file(path: str, rex: list) -> list:
+    """Extract Youtube video IDs from a given file path and regex dict
+
+    @param path Path to the video links file
+    @param rex List or tuple of regular expressions to use for searches
+    @return List of Youtube video IDs found in the file
+    """
+    data = read_file(path)
+    return extract_from(data, rex)
+
+
+def read_file(path: str) -> list:
+    """Read text links from the file at the given path
+
+    Removes comments, including trailing comments, and empty lines.
+
+    @param path Path to the text links file
+    @return List of links found within the file
+    """
+    with open(path, "r") as fh:
+        raw_data = fh.read().split("\n")
+
+    # The file _should_ end with a single newline, but could be malformed
+    if raw_data[-1] == "":
+        raw_data = raw_data[:-1]
+
+    # Trim whitespace on each line, if any
+    data_trimmed = map(lambda x: x.strip(), raw_data)
+
+    # Eliminate comment lines and empty lines
+    data_reduced = functools.reduce(reduce_comment_empty, data_trimmed, [])
+
+    # Eliminate trailing comments
+    data_cleaned = list(map(remove_comment, data_reduced))
+
+    return data_cleaned
+
+
+def reduce_comment_empty(acc: list, crt: list) -> list:
+    """Reduce text links data by removing comments and empty lines
+
+    @param acc Accumulator list
+    @param crt Current line to check
+    @return If checks passed, accumulator with `crt` appended; otherwise,
+    unchanged accumulator
+    """
+    if (not crt.startswith("#")) and (crt != ""):
+        acc.append(crt)
+    return acc
+
+
+def reduce_result(acc: list, crt: str, rex: list) -> list:
+    """Reduce a list of links based on regex searches
+
+    Assumes regular expressions in the list will feature the `id` match group.
+
+    @param acc Accumulator for storing results
+    @param crt Current link string
+    @param rex List or tuple of regular expressions
+    @return If any match found, accumulator with matching ID appended;
+    otherwise, unchanged accumulator
+    """
+    for r in rex:
+        if (m := r.search(crt)) is not None:
+            g = m.groupdict()
+            if g.get("id", None) is None:
+                raise RuntimeError("Regex missing `id` capture group")
+            acc.append(g.get("id"))
+            break
+    return acc
+
+
+def remove_comment(line: str) -> str:
+    """Remove trailing comments in a line, if any
+
+    @param line Line to check for trailing comments
+    @return If comments found, line without comments; otherwise, unchanged line
+    """
+    # Check for spaces first, e.g. "text  # comment here"
+    if len(spl := line.split(" ")) > 1:
+        return spl[0]
+    # Check for squashed comments, e.g. "text#comment"
+    if len(spl := line.split("#")) > 1:
+        return spl[0]
+    # Return unchanged
+    return line
+
+
+# Class wrapper
 # -----------------------------------------------------------------------------
 
 
 class TextLinkParser:
+    """Class-based wrapper for file contents"""
 
-    _rex = {
-        "normal": re.compile(r"(?<=[\?&]v=)(?P<id>[^\?&]+)"),
-        "short": re.compile(r"(?<=youtu\.be\/)(?P<id>[^\?&]+)")
-    }
+    _rex = REX_DEFAULT
 
-    def __init__(self, path=None):
-        if path is not None:
-            self.read_file(path)
+    def __init__(self, path: str):
+        self.path = path
 
-    # ---- Public methods ----
+    def extract(self) -> list:
+        """Extract video IDs from the given path
 
-    def read_file(self, path) -> None:
-        """Attempt to read the video links file
-
-        @param path File path to read and parse.
+        @return List of extracted video IDs
         """
-        try:
-            with open(path, "r") as fh:
-                self.data = self._store_data(fh.read().strip().split())
-        except FileNotFoundError as error:
-            t = f"File does not exist:  {path}"
-            Message(t, form="exit").print()
-            raise error
-        except (AttributeError, RuntimeError) as error:
-            t = "Unexpected input data format"
-            Message(t, form="exit").print()
-            raise error
+        self.data = extract_from_file(self.path, self._rex)
+        return self.data
 
-    # ---- Private methods ----
 
-    def _store_data(self, data) -> None:
-        """Parse the split text data and store in the instance
+# Main function
+# -----------------------------------------------------------------------------
 
-        Permits comments for lines starting with a hash or double-slash.
-        """
-        malformed = 0
-        video_ids = []
-        for value in data:
-            # Ignore empty lines and comments
-            if value == '':
-                continue
-            if value.startswith("#") or value.startswith("//"):
-                continue
-            # If a link is found by regex search, append
-            if (i := self._check_link(value)) is not None:
-                video_ids.append(i)
-                continue
-            # Increment malformed link text
-            malformed += 1
-        # Warn if at least one malformed link
-        if malformed > 0:
-            s = '' if malformed == 1 else "s"
-            t = f"{malformed} link{s} could not be identified"
-            Message(t, form="warn").print()
-        # Warn if no links found
-        if len(video_ids) == 0:
-            t = "No links found!"
-            Message(t, form="warn").print()
-        self.video_ids = video_ids
 
-    def _check_link(self, link) -> str:
-        """Check the given text for a link
+def main():
+    """Print text links found to stdout, separated by newline
 
-        Checks normal format or short format (youtu.be).
+    Dummy / Testing operations
+    """
+    parser = argparse.ArgumentParser()
 
-        @param link Link text to search within.
-        @return Video ID found, or None if nothing found.
-        """
-        if (r := self._rex.get("normal").search(link)) is not None:
-            return r.group("id")
-        if (r := self._rex.get("short").search(link)) is not None:
-            return r.group("id")
-        return None
+    parser.add_argument(
+        "file",
+        metavar="FILE",
+        type=str,
+        help="Path to the video links text file")
+
+    args = vars(parser.parse_args())
+
+    data = extract_from_file(args.get("file"), REX_DEFAULT)
+
+    if len(data) > 0:
+        for d in data:
+            print(d)
+
+
+# Entrypoint
+# -----------------------------------------------------------------------------
+
+
+if __name__ == "__main__":
+    main()
